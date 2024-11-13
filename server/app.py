41c57ff6ab07da19fb11581b99a00a6829f63076
Adrian Lariani <larianiadrian@gmail.com>
2024-11-13 13:00:56 -0500

    added function to keep server online with pings

--- server/app.py
+++ server/app.py
@@ -34,6 +34,9 @@ def predict():
 
     return jsonify({'prediction': str(round(prediction_probability))})
 
+@app.route('/keep_online', methods=['GET'])
+def ping():
+    return jsonify({"message": "Server is online"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
