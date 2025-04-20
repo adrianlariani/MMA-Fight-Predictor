a4a4d3c38376d30f30703f3fb61f0efd91c1c553
Adrian Lariani <larianiadrian@gmail.com>
2025-04-20 12:59:41 -0400

    Changed fighter data obtaining/parsing from next to python

--- client/app/api/predict/route.ts
+++ client/app/api/predict/route.ts
@@ -8,57 +8,7 @@ export async function POST(req: Request) {
   console.log(fighter1);
   console.log(fighter2);
 
-  const fighterOneStats = await getFighterByID(fighter1.value);
-  const fighterTwoStats = await getFighterByID(fighter2.value);
-
-  const stance_to_id: { [key: string]: number } = {
-    Orthodox: 0,
-    Southpaw: 1,
-    Switch: 2,
-    "Open Stance": 3,
-  };
-  console.log(fighterOneStats);
-  console.log(fighterTwoStats);
-
-  const weight_diff = fighterOneStats!.weight?.sub(fighterTwoStats!.weight!);
-  const height_diff = fighterOneStats!.height?.sub(fighterTwoStats!.height!);
-  const reach_diff = fighterOneStats!.reach?.sub(fighterTwoStats!.reach!);
-  const r_stance = stance_to_id[fighterOneStats?.stance!];
-  const b_stance = stance_to_id[fighterTwoStats?.stance!];
-  const slpm_total_diff = fighterOneStats!.slpm?.sub(fighterTwoStats!.slpm!);
-  const sig_str_acc_total_diff = fighterOneStats!.str_acc?.sub(
-    fighterTwoStats!.str_acc!
-  );
-  const sapm_total_diff = fighterOneStats!.sapm?.sub(fighterTwoStats!.sapm!);
-  const str_def_total_diff = fighterOneStats!.str_def?.sub(
-    fighterTwoStats!.str_def!
-  );
-  const td_avg_total_diff = fighterOneStats!.td_avg?.sub(
-    fighterTwoStats!.td_avg!
-  );
-  const td_acc_total_diff = fighterOneStats!.td_acc?.sub(
-    fighterTwoStats!.td_acc!
-  );
-  const td_def_total_diff = fighterOneStats!.td_def?.sub(
-    fighterTwoStats!.td_def!
-  );
-  const sub_avg_diff = fighterOneStats!.sub_avg?.sub(fighterTwoStats!.sub_avg!);
-
-  const features = [
-    weight_diff,
-    height_diff,
-    reach_diff,
-    r_stance,
-    b_stance,
-    slpm_total_diff,
-    sig_str_acc_total_diff,
-    sapm_total_diff,
-    str_def_total_diff,
-    td_avg_total_diff,
-    td_acc_total_diff,
-    td_def_total_diff,
-    sub_avg_diff,
-  ];
+  const fighters = [fighter1, fighter2];
 
   const response = await fetch(
     "https://mma-fight-predictor.onrender.com/predict",
@@ -67,17 +17,18 @@ export async function POST(req: Request) {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ features: features }),
+      body: JSON.stringify({ fighters: fighters }),
     }
   );
 
   const jsonResponse = await response.json();
   console.log(jsonResponse);
   console.log("Prediction: " + jsonResponse.prediction);
+
   const obj = {
     prediction: jsonResponse.prediction,
-    fighterOneName: fighterOneStats?.name,
-    fighterTwoName: fighterTwoStats?.name,
+    fighterOneName: fighter1.label,
+    fighterTwoName: fighter2.label,
   };
 
   const blob = new Blob([JSON.stringify(obj, null, 2)], {
--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
+import psycopg2
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import torch
 import torch.nn as nn
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -24,10 +26,12 @@ model.eval()
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
-    features = data['features']
+    ids = [fighter['value'] for fighter in data["fighters"]]
+    features = get_fighter_data_by_id(ids[0], ids[1])
+
     float_features = list(map(float, features))
     features_tensor = torch.tensor(float_features, dtype=torch.float32).unsqueeze(0)
-
+    print(features_tensor)
     with torch.no_grad():
         prediction = model(features_tensor)
         prediction_probability = prediction.item()
@@ -37,6 +41,50 @@ def predict():
 @app.route('/keep_online', methods=['GET'])
 def ping():
     return ('', 204)
+
+
+def get_fighter_data_by_id(id1, id2):
+      query = """SELECT
+                f1.weight - f2.weight AS weight_diff,
+                f1.height - f2.height AS height_diff,
+                f1.reach - f2.reach AS reach_diff,
+                CASE f1.stance
+                  WHEN 'Orthodox' THEN 0
+                  WHEN 'Southpaw' THEN 1
+                  WHEN 'Switch' THEN 2
+                  WHEN 'Open Stance' THEN 3
+                  ELSE 0
+                END AS r_stance,
+                CASE f2.stance
+                    WHEN 'Orthodox' THEN 0
+                    WHEN 'Southpaw' THEN 1
+                    WHEN 'Switch' THEN 2
+                    WHEN 'Open Stance' THEN 3
+                    ELSE 0
+                END AS b_stance,
+                f1.slpm - f2.slpm AS SLpM_total_diff,
+                f1.str_acc - f2.str_acc AS sig_str_acc_total_diff,
+                f1.sapm - f2.sapm AS SApM_total_diff,
+                f1.str_def - f2.str_def AS str_def_total_diff,
+                f1.td_avg - f2.td_avg AS td_avg_diff,
+                f1.td_acc - f2.td_acc AS td_acc_total_diff,
+                f1.td_def - f2.td_def AS td_def_total_diff,
+                f1.sub_avg - f2.sub_avg AS sub_avg_diff
+                FROM fighter_statistics f1
+                JOIN fighter_statistics f2
+                ON f1.id = %s AND f2.id = %s;"""
+      conn = psycopg2.connect(database=os.environ["DATABASE_NAME"],
+                          host=os.environ["DATABASE_HOST"],
+                          user=os.environ["DATABASE_USERNAME"],
+                          password=os.environ["DATABASE_PASSWORD"]
+                          )
+      cursor = conn.cursor()
+      cursor.execute(query, (id1, id2))
+      data = cursor.fetchone()
+      cursor.close()
+      return data
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
